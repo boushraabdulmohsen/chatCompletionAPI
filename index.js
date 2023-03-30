df8af1c66e649bcac244b463c613cea028fda9ea
Boushra Abdulmohsen <78104778+boushraabdulmohsen@users.noreply.github.com>
2023-03-30 20:52:03 +0000

    secure the key

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     organization: "org-CTNzTKu6zgT3hu4HsrBjtR84",
-    apiKey: "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"
+    apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
 
-async function createChat() {
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{
-            rule: "user", content: "Hello, World!"
-        }]
-    });
-    console.log(completion.data.choices[0].text);
-}
-
-createChat();
+const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{
+        rule: "user", content: "Hello, World!"
+    }]
+});
+console.log(completion.data.choices[0].message);
