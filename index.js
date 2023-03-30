91ab64bcc213f85691f37a370ee3ee2981e4cb5a
Boushra Abdulmohsen <78104778+boushraabdulmohsen@users.noreply.github.com>
2023-03-30 20:34:45 +0000

    add completion with helo world content

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import { Configuration, Configuration, OpenAIApi, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
-const Configuration = new Configuration({
-    organization: "",
-    apiKey: ""
+const configuration = new Configuration({
+    organization: "org-CTNzTKu6zgT3hu4HsrBjtR84",
+    apiKey: "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"
 });
 
-const OpenAIApi = new OpenAIApi(Configuration);
+const openai = new OpenAIApi(configuration);
+
+async function createChat() {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            rule: "user", content: "Hello, World!"
+        }]
+    });
+    console.log(completion.data.choices[0].text);
+}
+
+createChat();
