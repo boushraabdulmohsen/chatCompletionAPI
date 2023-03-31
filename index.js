e083ce0d17685c0cadb8a42398aad494d5887b91
Boushra Abdulmohsen <78104778+boushraabdulmohsen@users.noreply.github.com>
2023-03-31 16:28:57 +0000

    send request and set up the listener

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const configuration = new Configuration({
     organization: "org-CTNzTKu6zgT3hu4HsrBjtR84",
@@ -6,11 +9,24 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+const app = express();
+const port = 8090;
 
-const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{
-        rule: "user", content: "Hello, World!"
-    }]
+app.use(bodyParser.json());
+app.use(cors);
+
+app.get("/", async (req, res) => {
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            role: "user", content: "Hello, World!"
+        }]
+    })
+    res.json({
+        completion: completion.data.choices[0].message
+    });
 });
-console.log(completion.data.choices[0].message);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
