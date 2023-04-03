6e8d52d44c1eff10686058deae22174198bcb58c
Boushra Abdulmohsen <78104778+boushraabdulmohsen@users.noreply.github.com>
2023-04-03 14:29:48 +0000

    change the port and retrieve messages

--- index.js
+++ index.js
@@ -10,19 +10,20 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(cors);
 
 app.post("/", async (req, res) => {
-    const {message} = req.body;
+    const {messages} = req.body;
     
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{
-            role: "user", content: `${message}`
-        }]
+        messages: [
+            {"role": "system", "content": "You are a designed helpful chatGPT bot."},
+            ...messages
+        ]
     })
     res.json({
         completion: completion.data.choices[0].message
